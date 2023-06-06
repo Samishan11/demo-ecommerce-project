cd4315572ee6ff882f7998ae941b12b7b7e6a572
Samishan11 <samishanthapa0@gmail.com>
2023-06-06 17:21:30 +0545

    project complete

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,74 +1,67 @@
-import React from 'react'
-import { Link } from "react-router-dom"
-import {
-    Typography,
-    Button,
-    Input,
-} from "@material-tailwind/react";
-import { useDispatch, useSelector } from 'react-redux';
-import { toggleCart } from '../redux/slice/cartSlice';
-import { Box } from '@mui/material';
+import React from "react";
+import { Link } from "react-router-dom";
+import { Typography, Button, Input } from "@material-tailwind/react";
+import { useDispatch, useSelector } from "react-redux";
+import { toggleCart } from "../redux/slice/cartSlice";
+import { Box } from "@mui/material";
 export const Header: React.FC = () => {
-    const dispatch = useDispatch();
-    const { isShow: cartIsShow, cart } = useSelector((state: any) => state.cart);
+  const dispatch = useDispatch();
+  const { isShow: cartIsShow, cart } = useSelector((state: any) => state.cart);
 
-    const handelToggle = () => {
-        dispatch(toggleCart(!cartIsShow))
-    }
+  const handelToggle = () => {
+    dispatch(toggleCart(!cartIsShow));
+  };
 
-    return (
-        <Box
-            className=" w-full bg-gray-100 text-black py-3"
-        >
-            <Box className="flex flex-wrap items-center justify-between gap-y-4 text-gray-800">
-                <Link to="/">
-                    <Typography
-                        as="a"
-                        href="#"
-                        variant="h5"
-                        className="mr-4 ml-2 cursor-pointer py-1.5"
-                    >
-                        Demo-Ecommerce
-                    </Typography></Link>
+  return (
+    <Box className=" w-full bg-gray-100 text-black py-3">
+      <Box className="flex flex-wrap items-center justify-between gap-y-4 text-gray-800">
+        <Link to="/">
+          <Typography
+            as="a"
+            href="#"
+            variant="h5"
+            className="mr-4 ml-2 cursor-pointer py-1.5"
+          >
+            Demo-Ecommerce
+          </Typography>
+        </Link>
 
-                <Box className="relative hidden gap-2 md:block md:w-96">
-                    <Input
-                        type="search"
-                        color="black"
-                        label="Type here..."
-                        className="pr-20"
-                        containerProps={{
-                            className: "min-w-[288px]",
-                        }}
-                    />
-                    <Button
-                        size="sm"
-                        color="white"
-                        className="!absolute right-1 top-1 rounded"
-                    >
-                        Search
-                    </Button>
-                </Box>
-                <Box className="relative flex px-5 ">
-                    <Link
-                        to="/customer/order"
-                        className="mr-4 ml-2 cursor-pointer py-1.5"
-                    >
-                        My Order
-                    </Link>
-                    <Box className="relative">
-                        <i
-                            onClick={handelToggle}
-                            className=" rounded-full  border-gray-900 h-10 w-10 flex justify-center items-center fa-solid fa-bag-shopping bg-blue-gray-900/10 text-blue-gray-900 shadow-none hover:shadow-none hover:scale-105 focus:shadow-none focus:scale-105 active:scale-100"
-                        >
-
-                        </i>
-                        <span className="absolute font-serif top-0 right-2 text-sm">
-                            {cart?.length}
-                        </span>
-                    </Box>
-                </Box>
-            </Box>
+        <Box className="relative hidden gap-2 md:block md:w-96">
+          <Input
+            type="search"
+            color="black"
+            label="Type here..."
+            className="pr-20"
+            containerProps={{
+              className: "min-w-[288px]",
+            }}
+          />
+          <Button
+            size="sm"
+            color="white"
+            className="!absolute right-1 top-1 rounded"
+          >
+            Search
+          </Button>
         </Box>
-    )
-}
+        <Box className="relative flex pr-1 sm:px-5 ">
+          <Link
+            to="/customer/order"
+            className="mr-4 ml-2 cursor-pointer py-1.5"
+          >
+            My Order
+          </Link>
+          <Box className="relative">
+            <i
+              onClick={handelToggle}
+              className=" rounded-full  border-gray-900 h-10 w-10 flex justify-center items-center fa-solid fa-bag-shopping bg-blue-gray-900/10 text-blue-gray-900 shadow-none hover:shadow-none hover:scale-105 focus:shadow-none focus:scale-105 active:scale-100"
+            ></i>
+            <span className="absolute font-serif top-0 right-2 text-sm">
+              {cart?.length}
+            </span>
+          </Box>
+        </Box>
+      </Box>
+    </Box>
+  );
+};
--- src/page/productdetail/ProductDetail.tsx
+++ src/page/productdetail/ProductDetail.tsx
@@ -1,100 +1,136 @@
 import React, { useMemo, useEffect } from "react";
-import { useParams } from "react-router-dom"
+import { useParams } from "react-router-dom";
 import { Typography } from "@material-tailwind/react";
 import { useGetSingleProductQuery } from "../../redux/api/apiSlice";
 import { useDispatch, useSelector } from "react-redux";
 import { addToCart, updateCart } from "../../redux/slice/cartSlice";
 import { Box, Rating } from "@mui/material";
 import { LoadingSkeleton } from "../../components/LoadingSkeleton";
-import { v4 as uuid } from 'uuid';
-
+import { v4 as uuid } from "uuid";
 
 export const ProductDetail: React.FC = () => {
-    const { productId } = useParams();
-    // rtk hooks 
-    const dispatch = useDispatch();
-    const { cart } = useSelector((state: any) => state.cart)
-    // rtk query
-    const { data: product, isLoading } = useGetSingleProductQuery(String(productId));
-    // methods
-    const unique_id = uuid();
-    const handleAddToCart = (product: any) => {
-        const cartItem = {
-            id: unique_id.slice(0, 5),
-            pid: product.id,
-            title: product.title,
-            price: product.price,
-            quantity: Number(1),
-            image: product.image,
-        }
-        dispatch(addToCart(cartItem));
+  const { productId } = useParams();
+  // rtk hooks
+  const dispatch = useDispatch();
+  const { cart } = useSelector((state: any) => state.cart);
+  // rtk query
+  const { data: product, isLoading } = useGetSingleProductQuery(
+    String(productId)
+  );
+  // methods
+  const unique_id = uuid();
+  const handleAddToCart = (product: any) => {
+    const cartItem = {
+      id: unique_id.slice(0, 5),
+      pid: product.id,
+      title: product.title,
+      price: product.price,
+      quantity: Number(1),
+      image: product.image,
     };
-    const productInCart = useMemo(() => cart.find((data: any) => String(data.pid) === String(productId)), [cart, productId]);
-    const handleUpdateCart = (data: any) => {
-        const cData = { ...productInCart, ...data }
-        dispatch(updateCart(cData));
-    };
+    dispatch(addToCart(cartItem));
+  };
+  const productInCart = useMemo(
+    () => cart.find((data: any) => String(data.pid) === String(productId)),
+    [cart, productId]
+  );
+  const handleUpdateCart = (data: any) => {
+    const cData = { ...productInCart, ...data };
+    dispatch(updateCart(cData));
+  };
 
-    useEffect(() => {
-        window.scroll({ top: 0, })
-    }, [window])
+  useEffect(() => {
+    window.scroll({ top: 0 });
+  }, [window]);
 
-    return (
-        <Box className="flex flex-col sm:flex-row items-center sm:items-start justify-center py-12 2xl:px-20 md:px-6 px-4">
-            {!isLoading ? <Box className=" w-full sm:w-2/4 ">
-                <img className=" w-[80%] object-contain" alt={product?.title} src={product?.image} />
-            </Box>
-                :
-                <Box className="w-full md:w-2/4">
-                    <LoadingSkeleton className="w-[90%] mx-auto grid" height={"500px"} number={1} />
-                </Box>
-            }
-            {
-                !isLoading ?
-                    <Box className=" w-full md:w-2/4 lg:ml-8 md:ml-6 md:mt-0 mt-6">
-                        <Box className="border-b border-gray-200 pb-6">
-                            <Typography
-                                className=" lg:text-3xl text-2xl font-semibold lg:leading-6 leading-7 text-gray-800 mt-2">
-                                {product?.title}
-                            </Typography>
-                        </Box>
-                        <Box className="mt-5 flex items-center"
-                        >
-                            <Rating name="read-only" value={product?.rating?.rate} readOnly />
-                            <span className="ml-2">{product?.rating?.count} Reviews</span>
-                        </Box>
-                        <Typography className="mt-5 mr-5" variant="h3">${product?.price}</Typography>
-
-                        <Box>
-                            <Typography className=" text-base lg:leading-tight leading-normal text-gray-600 mt-7">{product?.description}</Typography>
-                            <Typography className="text-base leading-4 mt-7 text-gray-600">Product Code: 8BN321AF2IF0NYA</Typography>
-                            <Typography className="text-base leading-4 mt-4 text-gray-600">Length: 13.2 inches</Typography>
-                            <Typography className="text-base leading-4 mt-4 text-gray-600">Height: 10 inches</Typography>
-                            <Typography className="text-base leading-4 mt-4 text-gray-600">Depth: 5.1 inches</Typography>
-                            <Typography className="md:w-96 text-base leading-normal text-gray-600 mt-4">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Libero, eaque sequi aperiam magni sunt assumenda?</Typography>
-                        </Box>
-
-                        <Box className="mt-5" display={"flex"} alignItems={"center"} flexDirection={{ xs: "column", sm: "row" }}>
-                            {
-                                !productInCart ?
-                                    <button
-                                        onClick={() => handleAddToCart(product)}
-                                        className="mt-5 text-base flex items-center justify-center leading-none text-white bg-gray-900 w-full py-4 hover:bg-gray-800" >
-                                        Add To Cart
-                                    </button>
-                                    :
-                                    <button
-                                        onClick={() => handleUpdateCart({ id: productInCart?.id, quantity: productInCart.quantity + 1 })}
-                                        className="mt-5 text-base flex items-center justify-center leading-none text-white bg-gray-900 w-full py-4 hover:bg-gray-800">
-                                        Update Cart
-                                    </button>
-                            }
-                        </Box>
-                    </Box>
-                    :
-                    <Box className="w-full md:w-2/4">   <LoadingSkeleton className="w-[90%]" height={"100px"} number={5} /> </Box>
-            }
+  return (
+    <Box className="flex flex-col sm:flex-row items-center sm:items-start justify-center py-12 2xl:px-20 md:px-6 px-4">
+      {!isLoading ? (
+        <Box className=" w-full sm:w-2/4 ">
+          <img
+            className=" w-[80%] object-contain"
+            alt={product?.title}
+            src={product?.image}
+          />
         </Box>
-    );
-};
+      ) : (
+        <Box className="w-full md:w-2/4">
+          <LoadingSkeleton
+            className="w-[90%] mx-auto grid"
+            height={"500px"}
+            number={1}
+          />
+        </Box>
+      )}
+      {!isLoading ? (
+        <Box className=" w-full md:w-2/4 lg:ml-8 md:ml-6 md:mt-0 mt-6">
+          <Box className="border-b border-gray-200 pb-6">
+            <Typography className=" lg:text-3xl text-2xl font-semibold  text-gray-800 mt-2">
+              {product?.title}
+            </Typography>
+          </Box>
+          <Box className="mt-5 flex items-center">
+            <Rating name="read-only" value={product?.rating?.rate} readOnly />
+            <span className="ml-2">{product?.rating?.count} Reviews</span>
+          </Box>
+          <Typography className="mt-5 mr-5" variant="h3">
+            ${product?.price}
+          </Typography>
 
+          <Box>
+            <Typography className=" text-base lg:leading-tight leading-normal text-gray-600 mt-7">
+              {product?.description}
+            </Typography>
+            <Typography className="text-base leading-4 mt-7 text-gray-600">
+              Product Code: {unique_id}
+            </Typography>
+
+            <Typography className="md:w-96 text-base leading-normal text-gray-600 mt-4">
+              Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+              Exercitationem doloribus minus quasi, nam error enim magni
+              temporibus optio dolor mollitia nesciunt rerum corrupti est. Id
+              sequi dolorum libero atque odit.
+            </Typography>
+          </Box>
+
+          <Box
+            className="mt-2"
+            display={"flex"}
+            alignItems={"center"}
+            flexDirection={{ xs: "column", sm: "row" }}
+          >
+            {!productInCart ? (
+              <button
+                onClick={() => handleAddToCart(product)}
+                className="mt-5 text-base flex items-center justify-center leading-none text-white bg-gray-900 w-full py-4 hover:bg-gray-800"
+              >
+                Add To Cart
+              </button>
+            ) : (
+              <button
+                onClick={() =>
+                  handleUpdateCart({
+                    id: productInCart?.id,
+                    quantity: productInCart.quantity + 1,
+                  })
+                }
+                className="mt-5 text-base flex items-center justify-center leading-none text-white bg-gray-900 w-full py-4 hover:bg-gray-800"
+              >
+                Update Cart
+              </button>
+            )}
+          </Box>
+        </Box>
+      ) : (
+        <Box className="w-full md:w-2/4">
+          {" "}
+          <LoadingSkeleton
+            className="w-[90%]"
+            height={"100px"}
+            number={5}
+          />{" "}
+        </Box>
+      )}
+    </Box>
+  );
+};
