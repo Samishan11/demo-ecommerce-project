c47cb77dc3c4ae44b1eb323ce2929ffa3908cb31
Samishan11 <samishanthapa0@gmail.com>
2023-06-06 19:02:15 +0545

    final commit

--- src/route/Routing.tsx
+++ src/route/Routing.tsx
@@ -1,11 +1,11 @@
 import React, { ReactNode } from "react";
-import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { Header } from "../components/Header";
 import { ListProduct } from "../page/productlist/ListProduct";
 import { ProductDetail } from "../page/productdetail/ProductDetail";
 import { Cart } from "../page/cart/Cart";
-import { ToastContainer } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 import { useSelector } from "react-redux";
 import { Checkout } from "../page/checkout/Checkout";
 import { OrderViewPage } from "../page/order/OrderViewPage";
@@ -29,7 +29,14 @@ export const Routing: React.FC = () => {
         <Route path="/detail/:productId" element={<ProductDetail />} />
         <Route path="/customer/order" element={<OrderViewPage />} />
         <Route
-          path="/checkout" element={<Protected> <Checkout /></Protected>} />
+          path="/checkout"
+          element={
+            <Protected>
+              {" "}
+              <Checkout />
+            </Protected>
+          }
+        />
       </Routes>
       <ToastContainer />
     </Router>
